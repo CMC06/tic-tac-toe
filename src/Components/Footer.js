33d36228f83df2db77dcce79bb0e6ed02741ac7b
CMC06 <crzychelc@gmail.com>
2021-07-24 19:37:07 -0400

    adding conditional formatting for next player and instructions in footer, changing winner text placement

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,10 +1,10 @@
 const Footer = ({ player, newGame, winner, moveCount }) => {
   return (
     <footer className="footer">
-      { moveCount < 9 ? <p>Player {player ? 'O' : 'X'}, it's your turn!</p> : null}
-      <p className='smallP'>Can you mark 3 spaces in a row? The first player to mark 3 spaces in a row, wins!</p>
-      <button onClick={newGame}>New Game</button>
+      { winner === "There isn't a winner yet." ? <p>Player {player ? 'O' : 'X'}, it's your turn!</p> : null}
+      { winner === "There isn't a winner yet." ? <p className='smallP'>Can you mark 3 spaces in a row? The first player to mark 3 spaces in a row, wins!</p> : null}
       <p>{winner}</p>
+      <button onClick={newGame}>New Game</button>
     </footer>
   )
 }
